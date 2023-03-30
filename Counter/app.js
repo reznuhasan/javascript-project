250c3c14b0caecec8877101e70ceb40d77eaa7a4
reznuhasan <reznuhasan31@gmail.com>
2023-03-31 04:14:52 +0600

    by foreach

--- Counter/app.js
+++ Counter/app.js
@@ -1,19 +1,26 @@
-const increase=document.getElementById('increase');
-const decrease=document.getElementById('decrease');
-const reset=document.getElementById('reset')
+const btns=document.querySelectorAll('.btn');
+const count=document.getElementById('count-box');
+btns.forEach(btn=>{
+   btn.addEventListener('click',()=>{
+    const count=document.getElementById('count-box');
+    let value=parseInt(count.innerText);
+    console.log(typeof value)
+    if(btn.classList[1]==='increase'){
+        value+=1;
+    }else if(btn.classList[1]==="decrease"){
+        value-=1;
+    }else{
+        value=0;
+    }
 
-
-increase.addEventListener('click',()=>{
-    const count=document.getElementById('count-box');
-    const countNum=parseInt(count.innerText);
-    count.innerText=countNum+1;
-})
-decrease.addEventListener('click',()=>{
-    const count=document.getElementById('count-box');
-    const countNum=parseInt(count.innerText);
-    count.innerText=countNum-1;
-})
-reset.addEventListener('click',()=>{
-    const count=document.getElementById('count-box');
-    count.innerText=0;
+    if(value>0){
+        count.style.color='green'
+    }else if(value<0){
+        count.style.color='red'
+    }else{
+        count.style.color='black' 
+    }
+    count.innerText=value;
+    
+   })
 })
